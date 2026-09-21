8408e299c100fbcc12e8a3e17494991bfd7c2cc2
Thomcv3 <o2bafish@yahoo.com>
2026-09-20 22:21:21 -0400

    Complete Module 3 Spring Boot PostgreSQL project

---
+++ src/main/java/edu/fscj/cen3024c/taskmanager/Task.java
@@ -0,0 +1,68 @@
+package edu.fscj.cen3024c.taskmanager;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    private String status;
+
+    private LocalDate dueDate;
+
+    public Task() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
---
+++ src/main/java/edu/fscj/cen3024c/taskmanager/TaskRepository.java
@@ -0,0 +1,7 @@
+package edu.fscj.cen3024c.taskmanager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+
+}
